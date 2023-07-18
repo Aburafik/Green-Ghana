99a92d28e1b9cfb5c13b66f7f13f24e3f2527428
Rafik <rfkabubakari@gmail.com>
2023-07-18 19:27:43 +0000

    Search Trees screen added

--- lib/views/projects/registered_trees.dart
+++ lib/views/projects/registered_trees.dart
@@ -1,3 +1,4 @@
+import 'package:feather_icons/feather_icons.dart';
 import 'package:green_ghana_app/utils/exports.dart';
 
 class RegisterdTreesVC extends StatelessWidget {
@@ -8,6 +9,73 @@ class RegisterdTreesVC extends StatelessWidget {
     return Scaffold(
       appBar: commonAppBar(
         title: "Registerd Trees",
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+          children: [
+            const TextField(
+                decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+              prefixIcon: Icon(FeatherIcons.search),
+              labelText: "Search",
+            )),
+            const SizedBox(height: 15),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 20,
+                itemBuilder: (context, index) => Column(
+                  children: [
+                    Row(
+                      children: [
+                        const CircleAvatar(
+                          radius: 35,
+                          backgroundImage:
+                              AssetImage("assets/images/tree.jpeg"),
+                        ),
+                        const SizedBox(width: 10),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: const [
+                              Text(
+                                "Tree Name",
+                                style: TextStyle(
+                                    color: CustomColors.blackColor,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.symmetric(vertical: 5),
+                                child: Text(
+                                  "ID:5757575775",
+                                  style: TextStyle(
+                                      color: CustomColors.primaryColor,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                              ),
+                              Align(
+                                alignment: Alignment.centerRight,
+                                child: Text(
+                                  "12/07/2023",
+                                  style: TextStyle(),
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                    const Divider(
+                      thickness: 1,
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
